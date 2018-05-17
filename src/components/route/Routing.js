03b9977748f09a1fecc1fabfa83f7fed1028e9e3
Christian James <sijey@christian-linux-mint-sj12aries.Home>
2018-05-17 13:26:48 +0100

    initial commit

--- src/components/route/Routing.js
+++ src/components/route/Routing.js
@@ -1,22 +1,29 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom';
+import  {Router, Route, Link, Redirect} from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory'
 import Navbar from '../navbar/Navbar';
 import Home from '../home/Home';
 import About from '../about/About';
+import Services from '../services/Services';
 
+const history = createHistory();
+
+history.listen(() => {
+  window.scroll(0, 0)
+})
 
 const Routing = () => {
-  
-   return (
-     <Router>
-       <div>
-        <Navbar />
 
-         <Route exact path="/" component={ Home } />
-         <Route path="/about" component={ About } />
-       </div>
-     </Router>
-   )
+    return (
+      <Router history={history}>
+        <div>
+          <Navbar />
+          <Route exact path="/" component={ Home } />
+          <Route path="/about" component={ About } />
+          <Route path="/services" component={ Services } />
+        </div>
+      </Router>
+    )
   };
 
 
---
+++ src/components/services/Services.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './services.scss';
+
+// Icons 
+import ambulance from '../../assets/ambulance.png';
+
+const Services = () => {
+    return (
+        <div className="services">
+            <div className="container">
+                <div className="heading">
+                    <h1>
+                        Our Services
+                    </h1>
+                    <hr/>
+                </div>
+                <div className="section one">
+                  
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Services;
