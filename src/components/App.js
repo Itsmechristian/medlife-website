6693edc1ef7b28d0f0c57c15ae0fa5738e16492c
itsmechristian <hello@itsmechristian.co.uk>
2018-05-17 01:01:21 +0100

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 import Routing from './route/Routing';
 import './App.scss'
 
+import Home from './home/Home';
+import About from './about/About';
+
 class App extends Component {
 
   render() {
     return (
-      <Routing />
+        <Routing />
     )
   }
 }
--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,29 +1,93 @@
-import React, { Component } from 'react';
-import Imgpreload from '../preload/Imgpreload.js';
-import Bgpreload from '../preload/Bgpreload.js';
-
-import building from '../../assets/images/building.jpg';
-import buildingsmall from '../../assets/images/buildingsmall.jpg';
+import React, { Component } from "react";
+import Footer from '../footer/Footer';
+import Imgpreload from "../preload/Imgpreload.js";
+import Bgpreload from "../preload/Bgpreload.js";
 
 
-import './about.scss';
+import ambulanceCircle from '../../assets/images/ambulance-circle.png';
+import doctorCircle from '../../assets/images/doctor-circle.png';
+import medkitCircle from '../../assets/images/medkit-circle.png';
+import staffCircle from '../../assets/images/staff-circle.png';
+import stethoscopeCircle from '../../assets/images/stethoscope-circle.png';
+import wheelchairCircle from '../../assets/images/wheelchair-circle.png';
+import teamdoctor from '../../assets/images/teamdoctor.jpg';
 
+import "./about.scss";
 
 class About extends Component {
   render() {
     return (
       <div className="about">
-        <Bgpreload className="header" preloadimage={buildingsmall} hdimage={building}>
-          <div className="filter">
+        <div className="container">
+          <div className="heading">
+            <h1>About Us</h1>
+            <hr />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis
+              reprehenderit voluptas dolore excepturi, a doloribum.
+            </p>
           </div>
-            <div className="container">
-              <div className="text-wrapper">
-                <h1>About Us</h1>
+          <div className="section one">
+              <div className="content">
+                  <img src={ambulanceCircle} alt={ambulanceCircle}/>
+                  <div className="content-wrapper">
+                    <h2>Fast Ambulance Service</h2>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat, odit!</p>
+                  </div>
               </div>
-            </div>
-        </Bgpreload>
+              <div className="content">
+                  <img src={doctorCircle} alt={doctorCircle}/>
+                  <div className="content-wrapper">
+                    <h2>Our Best Doctor</h2>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat, odit!</p>
+                  </div>
+              </div>
+              <div className="content">
+                  <img src={medkitCircle} alt={medkitCircle}/>
+                  <div className="content-wrapper">
+                    <h2>Emergency Service</h2>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat, odit!</p>
+                  </div>
+              </div>
+              <div className="content">
+                  <img src={staffCircle} alt={staffCircle}/>
+                  <div className="content-wrapper">
+                    <h2>Qualified Staff</h2>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat, odit!</p>
+                  </div>
+              </div>
+              <div className="content">
+                  <img src={stethoscopeCircle} alt={stethoscopeCircle}/>
+                  <div className="content-wrapper">
+                    <h2>24/7 Service</h2>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat, odit!</p>
+                  </div>
+              </div>
+              <div className="content">
+                  <img src={wheelchairCircle} alt={wheelchairCircle}/>
+                  <div className="content-wrapper">
+                    <h2>Health Care</h2>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat, odit!</p>
+                  </div>
+              </div>
+          </div>
+          <div className="heading">
+            <h1>Our Team</h1>
+            <hr />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis
+              reprehenderit voluptas dolore excepturi, a doloribum.
+            </p>
+          </div>
+          <div className="section two">
+              <div className="content">
+                  <img src={teamdoctor} alt={teamdoctor}/>
+              </div>
+          </div>
+        </div>
+        <Footer />
       </div>
-    )
+    );
   }
 }
 
